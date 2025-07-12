e2dde222811d7fb6232ad6ff79cb035ea736a284
romanurajkin <UraykinRab@yandex.ru>
2025-07-12 23:46:56 +0300

    refactor: added interceptor and rule about pre-requests

--- core/network/src/main/java/com/example/network/internal/common/reRequest.kt
+++ core/network/src/main/java/com/example/network/internal/common/reRequest.kt
@@ -1,27 +1,26 @@
-package com.example.common
+package com.example.network.internal.common
 
+import io.ktor.client.call.HttpClientCall
+import io.ktor.client.plugins.Sender
+import io.ktor.client.request.HttpRequestBuilder
 import kotlinx.coroutines.delay
 
-suspend inline fun <reified T> reRequest(
-    block: () -> Result<T>
-): Result<T> {
+
+suspend fun Sender.reRequest(requestBuilder: HttpRequestBuilder): HttpClientCall {
     var count = 0
-    var result: Result<T> = Result.failure(UnknownException())
+    var call = execute(requestBuilder)
 
     while (count != 3) {
         delay(2000)
         count++
 
-        block().onSuccess {
-            result = Result.success(it)
-            count = 3
-        }.onFailure {
-            if (it !is ServerErrorException) {
-                result = Result.failure(it)
-                count = 3
-            }
+        if (call.response.status.value in 500..599) {
+            call = execute(requestBuilder)
+        }
+        else {
+            return call
         }
     }
 
-    return result
+    return call
 }
